fde038fa969ce6515c0fde96162e5692cffa9224
Jules Muhizi <jmuhizi@college.harvard.edu>
2022-10-22 21:07:05 +0000

    add qdense_batchnorm to supported layers

--- hls4ml/utils/config.py
+++ hls4ml/utils/config.py
@@ -107,7 +107,7 @@ def config_from_keras_model(model, granularity='model', default_precision='ap_fi
     norm_layers = ['BatchNormalization']
     activation_layers = ['Activation', 'LeakyReLU', 'ThresholdedReLU', 'ELU', 'PReLU', 'Softmax', 'ReLU']
     merge_layers = ['Add', 'Subtract', 'Multiply', 'Average', 'Maximum', 'Minimum', 'Concatenate', 'Dot']
-    qkeras_layers = ['QDense', 'QActivation', 'QConv1D', 'QConv2D', 'QBatchNormalization', 'QConv2DBatchnorm']
+    qkeras_layers = ['QDense', 'QDenseBatchnorm', 'QActivation', 'QConv1D', 'QConv2D', 'QBatchNormalization', 'QConv2DBatchnorm']
     upsampling_layers = ['UpSampling1D', 'UpSampling2D']
     reshaping_layers = ['ZeroPadding1D', 'ZeroPadding2D']
     graph_layers = ['GarNet', 'GarNetStack']
